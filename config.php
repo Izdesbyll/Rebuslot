adfeab56369be264587a061264de20bd5e5af497
Izdesbyll <48257488+Izdesbyll@users.noreply.github.com>
2020-02-27 17:37:41 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -16,17 +16,27 @@ mysqli_select_db($conn, "vocabulary");
 
 echo " Database vocabulary is selected successfully ";
 
-$arr_serialize1=explode(',',$_POST['arr_serialize1']);
-$arr_serialize2=explode(',',$_POST['arr_serialize2']);
-$arr_serialize3=explode(',',$_POST['arr_serialize3']);
-$arr_serialize4=explode(',',$_POST['arr_serialize4']);
-$arr_serialize5=explode(',',$_POST['arr_serialize5']);
-$arr_serialize6=explode(',',$_POST['arr_serialize6']);
-print_r($arr_serialize1);
-print_r($arr_serialize2);
-print_r($arr_serialize3);
-print_r($arr_serialize4);
-print_r($arr_serialize5);
-print_r($arr_serialize6);
+$arr_serialize7=explode(',',$_POST['arr_serialize1']);
+$arr_serialize8=explode(',',$_POST['arr_serialize2']);
+$arr_serialize9=explode(',',$_POST['arr_serialize3']);
+$arr_serialize10=explode(',',$_POST['arr_serialize4']);
+$arr_serialize11=explode(',',$_POST['arr_serialize5']);
+$arr_serialize12=explode(',',$_POST['arr_serialize6']);
+print_r($arr_serialize7);
+print_r($arr_serialize8);
+print_r($arr_serialize9);
+print_r($arr_serialize10);
+print_r($arr_serialize11);
+print_r($arr_serialize12);
+$arr_serialize1=implode(" , ",$arr_serialize7);
+$arr_serialize2=implode(" , ",$arr_serialize8);
+$arr_serialize3=implode(" , ",$arr_serialize9);
+$arr_serialize4=implode(" , ",$arr_serialize10);
+$arr_serialize5=implode(" , ",$arr_serialize11);
+$arr_serialize6=implode(" , ",$arr_serialize12);
+
+
+$sql = "INSERT INTO contents_arr(arr_serialize1, arr_serialize2, arr_serialize3, arr_serialize4, arr_serialize5, arr_serialize6) VALUES('".$arr_serialize1."','".$arr_serialize2."','".$arr_serialize3."','".$arr_serialize4."','".$arr_serialize5."','".$arr_serialize6."')";
+mysqli_query($conn,$sql);
 ?> 
 	
